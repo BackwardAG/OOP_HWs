49ec4e7e053b84393433288630368f566f908982
Armen G <ar.gukasov@gmail.com>
2023-09-29 14:27:25 +0300

    HWs/HW_004/Main.java

---
+++ HWs/HW_005/Main.java
@@ -0,0 +1,11 @@
+package HWs.HW_005;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorPresenter presenter = new CalculatorPresenter(model, view);
+        presenter.onAddButtonClicked();
+    }
+}
